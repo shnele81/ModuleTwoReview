1aaf742706ef35d0aa2705533ff2f7f7ea02cbfd
Andrew ONeill <andy@evolve757.com>
2025-09-18 08:00:07 -0400

    first part of module two review exercise

---
+++ ModuleTwoReview/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ModuleTwoReview;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
---
+++ ModuleTwoReview/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace ModuleTwoReview;
+
+public class Student
+{
+    public string Name { get; set; }
+    public int ID { get; set; }
+    public List<double> Grades { get; set; } =  new List<double>(); //created the 3 parameters
+
+    public void AddGrade(double grade) //created the first method
+    {
+        Grades.Add(grade);
+    }
+
+    public void AddGrade(params double [] grades) //created the second method
+    {
+        Grades.AddRange(grades);
+    }
+
+}
