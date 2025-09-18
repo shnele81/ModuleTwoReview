2918e9668b85fb2e307688563e5b7e534c31b3ec
Andrew ONeill <andy@evolve757.com>
2025-09-18 08:57:29 -0400

    third part of exercise completed

--- ModuleTwoReview/Program.cs
+++ ModuleTwoReview/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        List<Student> students = new List<Student>();
+        foreach (var studentOne in students)
+        {
+            studentOne.Name = "Brad";
+            studentOne.ID = 1;
+        }
+
+        foreach (var studentTwo in students)
+        {
+            studentTwo.Name = "Stacy";
+            studentTwo.ID = 2;
+        }
+
+        foreach (var studentThree in students)
+        {
+            studentThree.Name = "Frank";
+            studentThree.ID = 3;
+        }
+
+        foreach (var studentFour in students)
+        {
+            studentFour.Name = "Beth";
+            studentFour.ID = 4;
+        }
+        //created 4 instances of students and gave them names and ID numbers. Commit work spot.
+
     }
 }
